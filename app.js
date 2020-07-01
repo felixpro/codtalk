f68ff3e3ae2b900dc600cbc869aeb3478eff6823
Felix Pujols <pujols.fr@gmail.com>
2020-07-01 11:53:00 -0400

    Config of express-hdb, partials, bootstrap and mongoose

--- app.js
+++ app.js
@@ -6,20 +6,30 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
+var exphbs  = require('express-handlebars');
+
+
+
 var app = express();
-
-
-
 // mongoDB config
-var mongoDB = ' mongodb+srv://fpuj:f2367564@codtalk.fvhwn.mongodb.net/<dbname>?retryWrites=true&w=majority';
-mongoose.connect (mongoDB);
+var mongoDB = ' mongodb://fpuj:f2367564@codtalk-shard-00-00.fvhwn.mongodb.net:27017,codtalk-shard-00-01.fvhwn.mongodb.net:27017,codtalk-shard-00-02.fvhwn.mongodb.net:27017/codTalk?ssl=true&replicaSet=atlas-12uwqv-shard-0&authSource=admin&retryWrites=true&w=majority';
+mongoose.connect (mongoDB, { useNewUrlParser: true }, function(err) {
+  if (err) {
+    console.log("Error when connection to DB")
+  }
+  console.log("db working perfect")
+});
 mongoose.Promise = global.Promise;
-var db = mongoose.conexión;
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', exphbs({
+  defaultLayout: 'layout',
+  extname: '.hbs'
+}))
 
+app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
